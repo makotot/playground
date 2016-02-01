234b526215ba2685935a1262cd4cce162ebe14c6
makotot <makoto.ttn@gmail.com>
2016-02-01 15:11:20 +0900

    + test

--- react-testing/src/js/app.js
+++ react-testing/src/js/app.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Foo } from './components/foo'
+import { Bar } from './components/bar'
 
 ReactDOM.render(<Foo />, document.getElementById('app'))
+ReactDOM.render(<Bar />, document.getElementById('nav'))
---
+++ react-testing/src/js/components/bar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+export class Bar extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <ul className="bar__list">
+      </ul>
+    )
+  }
+}
