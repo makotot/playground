8b9ef3e3eb9f6c129854fedafdcb83ffe6b1b557
makotot <makoto.ttn@gmail.com>
2016-04-17 22:12:17 +0900

    update

--- rails/til/db/schema.rb
+++ rails/til/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417035002) do
+ActiveRecord::Schema.define(version: 20160417130948) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "body",       limit: 255
+    t.string   "tag",        limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.string   "tag",        limit: 255
   end
 
 end
--- rails/til/test/controllers/posts_controller_test.rb
+++ rails/til/test/controllers/posts_controller_test.rb
@@ -18,7 +18,7 @@ class PostsControllerTest < ActionController::TestCase
 
   test "should create post" do
     assert_difference('Post.count') do
-      post :create, post: { body: @post.body, title: @post.title }
+      post :create, post: { body: @post.body, tag: @post.tag, title: @post.title }
     end
 
     assert_redirected_to post_path(assigns(:post))
@@ -35,7 +35,7 @@ class PostsControllerTest < ActionController::TestCase
   end
 
   test "should update post" do
-    patch :update, id: @post, post: { body: @post.body, title: @post.title }
+    patch :update, id: @post, post: { body: @post.body, tag: @post.tag, title: @post.title }
     assert_redirected_to post_path(assigns(:post))
   end
 
