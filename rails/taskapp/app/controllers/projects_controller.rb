9a6d6490497916d21f0dd18a698e2df45a5458d8
makotot <makoto.ttn@gmail.com>
2016-05-03 23:00:02 +0900

    + redirect_to

--- rails/taskapp/app/controllers/projects_controller.rb
+++ rails/taskapp/app/controllers/projects_controller.rb
@@ -14,9 +14,11 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.save
-
-    redirect_to projects_path
+    if @project.save
+      redirect_to projects_path
+    else
+      render 'new'
+    end
   end
 
   private
