d9b75587d55b9e8bd74027a8ca5ff4038d708fbe
makotot <makoto.ttn@gmail.com>
2016-05-31 01:08:53 +0900

    * activerecord

--- activerecord/main.rb
+++ activerecord/main.rb
@@ -1,11 +1,15 @@
 require 'active_record'
+require 'logger'
 
 ActiveRecord::Base.establish_connection(
   "adapter" => "sqlite3",
   "database" => "./blog.db"
 )
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 class Post < ActiveRecord::Base
+  scope :top3, -> { order("created_at").limit(3) }
 end
 
 #post = Post.new(:title => "title1", :body => "hello1")
@@ -34,4 +38,14 @@ end
 #p Post.where("title = ? and id = ?", "title1", 1)
 #p Post.where("title = :title and id = :id", {:title => "title1", :id => 1})
 #p Post.where("id > ?", 2)
-p Post.where("body like ?", "hello%")
+#p Post.where("body like ?", "hello%")
+#p Post.where(:id => 1..3)
+#p Post.where(:id => [1, 3])
+#p Post.order("id desc")
+#p Post.order("id desc").limit(3)
+#p Post.top3
+#Post.where(:title => "title5").first_or_create
+Post.where(:title => "title6").first_or_create do |p|
+  p.body = "hello6"
+end
+p Post.all
