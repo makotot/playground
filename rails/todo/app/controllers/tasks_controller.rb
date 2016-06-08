997d685c32e48e81fe86934349d38e9b1662c5e2
makotot <makoto.ttn@gmail.com>
2016-06-09 01:47:35 +0900

    * todo

--- rails/todo/app/controllers/tasks_controller.rb
+++ rails/todo/app/controllers/tasks_controller.rb
@@ -2,4 +2,15 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :note, :completed)
+  end
 end
