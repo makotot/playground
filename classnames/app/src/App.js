fe88ccee1bd2f1245679db1afd0518f7e39b42b7
makotot <makoto.ttn@gmail.com>
2018-08-01 01:07:38 +0900

    update

--- classnames/app/src/App.js
+++ classnames/app/src/App.js
@@ -52,16 +52,7 @@ class App extends Component {
 
         <Growed>
           <GrowedItem grow={ 1 }>
-            <Box>1</Box>
-          </GrowedItem>
-          <GrowedItem grow={ 4 }>
-            <Box>4</Box>
-          </GrowedItem>
-        </Growed>
-
-        <Growed>
-          <GrowedItem grow={ 2 }>
-            <Box>2</Box>
+            <Box>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Box>
           </GrowedItem>
           <GrowedItem grow={ 3 }>
             <Box>3</Box>
